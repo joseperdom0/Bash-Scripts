8960cd6c3650f06da67f9badaf3a292bc754c817
joseperdom0 <aguilar.roy@gmail.com>
2022-11-12 21:10:17 -0500

    Added local IP sweeper

---
+++ local_ip_sweeper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will ping all local IP's once and will show the ones active (or responding)
+
+for i in {2..2}
+do
+	for j in {1..255}
+	do
+		ping -c 1 192.168.$i.$j | grep "64 bytes"&
+done
+done
