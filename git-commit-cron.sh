ef47f7a98477b6be8e19d5f87520d78dbb2997c9
joseperdom0 <aguilar.roy@gmail.com>
2022-11-13 20:41:23 -0500

    cron commit @ Sun Nov 13 08:41:23 PM EST 2022

--- git-commit-cron.sh
+++ git-commit-cron.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+times=$(shuf -i 3-7 -n 1)
+echo $times
+for i in {1..$(times)}
+do
 now=$(date)
-log=$(echo "Daily back-up / Time of commit: $now")
+log=$(echo -e "Daily back-up / Time of commit: $now  ")
 echo "$log" >> cron.log
 git add .
 git commit -m "cron commit @ $now"
 git push
-
+sleep 3
+done
