b82f6d3a3f92af0b55c260058baf1787a1b618af
joseperdom0 <aguilar.roy@gmail.com>
2022-11-13 20:04:02 -0500

    cron commit @ Sun Nov 13 08:04:02 PM EST 2022

--- git-commit-cron.sh
+++ git-commit-cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 now=$(date)
-log=(echo -e "Time of commit $now")
-cat $log >>cron.log
+log=$(echo -e "Time of commit $now")
+cat $log >> cron.log
 git add .
 git commit -m "cron commit @ $now"
 git push
