88fe165641df6274c5284b416091c177d8125f3d
rangda <rangda@rangda.kali>
2022-11-29 22:03:39 -0500

    Automatic Commit, change detected

--- git-push-all-repos.sh
+++ git-push-all-repos.sh
@@ -5,8 +5,11 @@
 # This script allows copying all initial repositories to work in my local machine
 
 
+now=$date
 cat list-of-repos.txt | while read line
 do
-	cd ~
-	git clone git@github.com:joseperdom0/$line
+	cd ~/$line
+	git add .
+	git commit -m "Automatic Commit, change detected $now"
+	git clone push
 done
